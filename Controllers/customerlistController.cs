c9788aa03a5542e326be51a688c24dfb2232b897
indupriya968 <yarramaneniindu@gmail.com>
2022-09-08 11:47:14 +0530

    Add project files.

---
+++ Controllers/customerlistController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using customerlistcapstone.models;
+
+namespace customerlistcapstone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class customerlistController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public customerlistController(IConfiguration configuration)
+
+        {
+            _configuration = configuration;
+        }
+
+
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                           select name,password,email from
+                           dbo.customerlist";
+
+
+            DataTable table = new();
+            string sqlDataSource = _configuration.GetConnectionString("customerlistAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+
+        }
+        [HttpPost]
+
+        public JsonResult Post(customerlist dep)
+        {
+            string query = @"
+                           insert into dbo.customerlist
+                           values (@name,@password,@email) 
+                            ";
+
+            DataTable table = new();
+            string sqlDataSource = _configuration.GetConnectionString("customerlistAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new(sqlDataSource))
+            {
+                {
+                    myCon.Open();
+                    using SqlCommand myCommand = new(query, myCon);
+                   
+                    myCommand.Parameters.AddWithValue("@name", dep.name);
+                    myCommand.Parameters.AddWithValue("@password", dep.password);
+                    myCommand.Parameters.AddWithValue("@email", dep.email);
+
+
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+
+        }
+
+    }
+}
---
+++ models/customerlist.cs
@@ -0,0 +1,12 @@
+﻿namespace customerlistcapstone.models
+{
+    public class customerlist
+    {
+       
+        public string? name { get; set; }
+
+        public string? password { get; set; }
+
+        public string? email { get; set; }
+    }
+}
